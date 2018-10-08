ddd42ffa6a7f9bdebb2fe17fe451bc7ec78912e4
Eduardo Alexis Sandoval Aparicio <43801972+lexusui1@users.noreply.github.com>
2018-10-07 22:05:40 -0600

    Add files via upload

---
+++ Clase6.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio6;
+
+/**
+ *
+ * @author Lex
+ */
+public class Clase6 {
+    String nombre;
+    float conductividad_electrica;
+    float conductividad_termica;
+}
